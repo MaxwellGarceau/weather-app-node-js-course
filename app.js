32ad700a577094faadb3f3356a92b0c58896987e
Max Garceau <maxwellgarceau@gmail.com>
2018-07-08 11:36:09 -0500

    Encoding User Input and Dynamic HTTP Requests

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+// console.log(argv.address);
+const encodedAddress = encodeURIComponent(argv.address);
+// console.log(encodedAddress);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   headers: {
     'key': 'AIzaSyA4IoueWgxRuPVsUNo7sAPvxetpeUoiQSQ'
   },
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
+  // console.log(JSON.stringify(response, undefined, 2));
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
